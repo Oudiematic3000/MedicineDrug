43fa1bbba370640c83f57a6cabc6fd4dda10dee2
Oudiematic3000 <oudmayera@gmail.com>
2026-02-17 15:12:52 +0200

    Class example

---
+++ MedicineDrug/Assets/Scripts/Players1Keyboard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Players1Keyboard : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
