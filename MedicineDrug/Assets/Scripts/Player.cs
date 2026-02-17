efeac8e0f2c8ec55577de5a11f35ab8c4794d302
Oudiematic3000 <oudmayera@gmail.com>
2026-02-17 17:35:05 +0200

    PlayerController

---
+++ MedicineDrug/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+public class Player : MonoBehaviour, IGameplayActions
+{
+    Rigidbody rb;
+
+    Vector2 moveInput;
+
+    public float rotationTime, moveSpeed, accelerationTime;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Move();
+    }
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLock(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        moveInput = context.ReadValue<Vector2>();
+        if (context.canceled) moveInput = Vector2.zero;
+    }
+
+    public void OnPickUp(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Move()
+    {
+        if(moveInput==Vector2.zero)return;
+        Vector3 moveInput3 = new Vector3(moveInput.x, 0, moveInput.y);
+        Quaternion targetRotation = Quaternion.LookRotation(moveInput3);
+        transform.rotation =Quaternion.Slerp(transform.rotation, targetRotation,rotationTime*Time.deltaTime);
+
+        rb.linearVelocity = Vector3.Lerp(transform.forward, moveInput3 * moveSpeed, accelerationTime*Time.deltaTime);
+    }
+}
