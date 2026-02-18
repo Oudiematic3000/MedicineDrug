f042a1dd6f234470e72792262e1448b734e47200
Oudiematic3000 <oudmayera@gmail.com>
2026-02-18 08:15:17 +0200

    Grab hitbox and Usable Base class

---
+++ MedicineDrug/Assets/Scripts/GrabHitbox.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GrabHitbox : MonoBehaviour
+{
+    Player player;
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Usable usable;
+        if (!other.GetComponent<Usable>()) return;
+        else usable = other.GetComponent<Usable>();
+    }
+}
---
+++ MedicineDrug/Assets/Scripts/Usable.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Usable : MonoBehaviour
+{
+    //replace with 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
