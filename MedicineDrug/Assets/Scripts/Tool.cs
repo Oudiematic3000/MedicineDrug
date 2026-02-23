a41a80c3d0e316029eb4cccdb4687e3950f5e8e5
Oudiematic3000 <oudmayera@gmail.com>
2026-02-23 15:45:20 +0200

    Trolley final changes

--- MedicineDrug/Assets/Scripts/Tool.cs
+++ MedicineDrug/Assets/Scripts/Tool.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class Tool : Usable
 {
     public bool droppableOnFloor=false;
+
     public virtual void OnPickup(Player player)
     {
         print("Picked up!");
     }
 
     public virtual void OnPutDown(Player player)
-    //[SerializeField] public UsableTemplate template;
-    
+    {
+
+    }
     public void OnPickup()
     {
         print("Interact Time is:" + template.interactTime);
