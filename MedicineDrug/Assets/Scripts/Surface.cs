c57bb2786af4ea177f9dff4f07627a0519348ba1
Oudiematic3000 <oudmayera@gmail.com>
2026-03-05 15:03:28 +0200

    Surface

---
+++ MedicineDrug/Assets/Scripts/Surface.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Surface : Usable
+{
+    public Tool placedTool;
+    public Transform toolSlot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickUpPutDown(Player player)
+    {
+        if (placedTool) Pickup(player);
+        else Putdown(player);
+    }
+    void Pickup(Player player)
+    {
+        if(player.heldTool)return;
+
+        placedTool.transform.SetParent(player.hand);
+        placedTool.transform.localPosition = Vector3.zero;
+        placedTool.transform.rotation =Quaternion.LookRotation(player.hand.forward, Vector3.up);
+        placedTool = null;
+    }
+    void Putdown(Player player)
+    {
+        if (!player.heldTool) return;
+        player.heldTool.transform.SetParent(toolSlot);
+        player.heldTool.transform.localPosition = Vector3.zero;
+        player.transform.rotation = Quaternion.LookRotation(toolSlot.forward, Vector3.up);
+        player.heldTool = null;
+    }
+}
