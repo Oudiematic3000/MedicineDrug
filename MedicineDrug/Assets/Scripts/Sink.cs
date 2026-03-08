478859bc94deb389ab1f7e52011f965615d937c5
Simon Du Plooy <47170836+SimonDuPlooyGit@users.noreply.github.com>
2026-03-08 17:50:29 +0200

    Sink makes you clean!

--- MedicineDrug/Assets/Scripts/Sink.cs
+++ MedicineDrug/Assets/Scripts/Sink.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Sink : Interactable
 {
+    private Player player;
     
+    public override void OnInteract(bool action, Player player)
+    {
+        base.OnInteract(action, player);
+        this.player = player;
+    }
+    
+    
+    public override void OnComplete()
+    {
+        player.GetComponent<Player>().makeClean();
+    }
 }
