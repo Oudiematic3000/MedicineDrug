600c2367466156bb91d9238aff85a22673395669
Oudiematic3000 <oudmayera@gmail.com>
2026-03-08 15:56:19 +0200

    DoorSound fix

--- MedicineDrug/Assets/Scripts/DoorAudio.cs
+++ MedicineDrug/Assets/Scripts/DoorAudio.cs
@@ -5,19 +5,20 @@ public class DoorAudio : MonoBehaviour
 {
     public AudioClip openSound;
     public AudioClip slamSound;
-    
+    public bool cooldown=false;
     public void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Door collider was hit");
-        
+        if (cooldown) return;
+
         if (other.gameObject.GetComponentInChildren<Player>() != null)
         {
-            Debug.Log("Player opened door");
-            AudioManager.instance.PlaySFX(openSound);
+            AudioManager.instance.PlaySFX(openSound, 0.3f);
+            
         } else if (other.gameObject.GetComponentInChildren<Trolley>() != null || other.gameObject.GetComponentInChildren<GurneyHandle>())
         {
-            Debug.Log("Gurney slammed door");
-            AudioManager.instance.PlaySFX(slamSound);
+            AudioManager.instance.PlaySFX(slamSound, 0.3f);
         }
+        cooldown = true;
+        LeanTween.delayedCall(0.8f,() => { cooldown = false; });
     }
 }
